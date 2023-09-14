436ec92955c5ee68594e9f7e4b79f993fbd1f64c
ronanj <ronan.jezequel@gmail.com>
2023-09-14 18:11:28 +0800

    make SSE context public

--- sse.go
+++ sse.go
@@ -7,51 +7,51 @@ import (
 	"net/http"
 )
 
-type tinSSEContext struct {
+type SSEContext struct {
 	*Context
 	flusher http.Flusher
 }
 
-func (t *Context) SSE() *tinSSEContext {
+func (t *Context) SSE() *SSEContext {
 
 	flusher, ok := t.Writer.(http.Flusher)
 
 	if !ok {
 		http.Error(t.Writer, "Streaming unsupported!", http.StatusInternalServerError)
-		return &tinSSEContext{Context: t}
+		return &SSEContext{Context: t}
 	}
 
-	t.Writer.Header().Set("Content-Type", "text/event-stream")
+	t.Writer.Header().Set("Content-Type", "text/event-stream; charset=utf-8")
 	t.Writer.Header().Set("Cache-Control", "no-cache")
 	t.Writer.Header().Set("Connection", "keep-alive")
 	t.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 
-	return &tinSSEContext{
+	return &SSEContext{
 		flusher: flusher,
 		Context: t,
 	}
 }
 
-func (t *tinSSEContext) Error(err error) error {
+func (t *SSEContext) Error(err error) error {
 
 	return t.send("error", err.Error())
 }
 
-func (t *tinSSEContext) Event(event string) error {
+func (t *SSEContext) Event(event string) error {
 
 	return t.send(event, nil)
 }
 
-func (t *tinSSEContext) JSON(v interface{}) error {
+func (t *SSEContext) JSON(v interface{}) error {
 
 	return t.send("data", v)
 }
 
-func (t *tinSSEContext) Send(event string, v interface{}) error {
+func (t *SSEContext) Send(event string, v interface{}) error {
 	return t.send(event, v)
 }
 
-func (t *tinSSEContext) send(event string, v interface{}) error {
+func (t *SSEContext) send(event string, v interface{}) error {
 
 	if t.clientGone || t.flusher == nil {
 		return errors.New("client is gone")
@@ -78,6 +78,6 @@ func (t *tinSSEContext) send(event string, v interface{}) error {
 	return nil
 }
 
-func (t *tinSSEContext) Gone() <-chan struct{} {
+func (t *SSEContext) Gone() <-chan struct{} {
 	return t.Request.Context().Done()
 }
